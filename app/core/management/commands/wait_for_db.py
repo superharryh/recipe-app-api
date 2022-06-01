91fe1a089473593817ed846315894ae9de4d6da3
Игорь <pupik.1985@list.ru>
2022-06-02 00:18:04 +1000

    configure linting

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,32 +3,21 @@ Django command to wait for the database to be available.
 """
 
 import time
-
 from psycopg2 import OperationalError as Psycopg2OpError
-
-from django.db.utils import OperationalError # Error Django throws when database is not ready
-
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
     """Django command to wait for the database."""
-
     def handle(self, *args, **options):
         """"Entrypoint for command."""
-        self.stdout.write('Waiting for database...') # this just shows in our console the message waiting for database.
+        self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default']) # by this .check command we check if database ready or not
-                # This is that .check method that we mock inside our tests/test_commands.py
-                
-                db_up = True # as soon as it gets marked as true, 
-                # then the while loop will stop because db_up is no longer False and then it will execute line 33
-
-            except (Psycopg2OpError, OperationalError): # if database is not ready we raise an exception
+                self.check(databases=['default'])
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-
         self.stdout.write(self.style.SUCCESS('Database available!'))
-
